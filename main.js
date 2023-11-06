b062970d249198a500e8d564c41b128269bfeba3
Amanda <amanda.ausejo@gmail.com>
2023-11-06 15:17:30 +0100

    feat 1

---
+++ main.js
@@ -0,0 +1,5 @@
+//Esercizio 1: Crea una arrow functio che accetta due numeri come parametri e
+// restituisce la loro sottrazione.
+const sottrazioneNumeri = (a, b) => a - b;
+let risultato = sottrazioneNumeri(55, 10);
+console.log(risultato);
