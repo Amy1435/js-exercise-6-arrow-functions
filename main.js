e8fd7ab39a28ad5da00b63fc96c356bc69a783bb
Amanda <amanda.ausejo@gmail.com>
2023-11-06 16:37:49 +0100

    feat 3 & 4

--- main.js
+++ main.js
@@ -8,17 +8,43 @@
 //Dovrebbe avere proprietà per 'nome' e 'età'.
 //Dovrebbe anche avere un metodo che usa la parola chiave 'this' per stampare il nome e l'età dello studente e un metodo per incrementare l'età.
 
-const studente = {
-    nome: "Stefania",
-    eta: 24,
-    presentazioneBase: function () {
-        return `Hello, my name is ${this.nome}, and I'm ${this.eta} years old.`;
-    },
+// const studente = {
+//     nome: "Stefania",
+//     eta: 24,
+//     presentazioneBase: function () {
+//         return `Hello, my name is ${this.nome}, and I'm ${this.eta} years old.`;
+//     },
 
-    incrementaEta: function (eta) {
-        return (this.eta += eta);
-    },
-};
+//     incrementaEta: function (eta) {
+//         return (this.eta += eta);
+//     },
+// };
 
-console.log(studente.presentazioneBase());
-console.log(studente.incrementaEta(1));
+// console.log(studente.presentazioneBase());
+// console.log(studente.incrementaEta(1));
+
+//Esercizio 3: Crea una semplice pagina HTML con un elemento div.
+//Usa JavaScript per cambiare l'innerHTML e l'innerText del div.
+
+// window.addEventListener("load", () => {
+//     const divContainer = document.getElementById("container");
+//     divContainer.innerHTML = "Inner Html";
+//     divContainer.innerText = "Inner Text";
+//     console.log(divContainer.innerHTML);
+//     console.log(divContainer.innerText);
+// });
+
+//Esercizio 4: Crea una lista non ordinata con tre elementi di lista.
+//Usa JavaScript per aggiungere un nuovo elemento di lista alla fine della lista usando appendChild.
+//Poi usa insertBefore per aggiungere un elemento di lista all'inizio della lista.
+
+window.addEventListener("load", () => {
+    const container = document.getElementById("lista");
+    const firstLi = container.querySelector("li");
+    const ultimoLi = document.createElement("li");
+    const primoLi = document.createElement("li");
+    ultimoLi.innerText = "pomodoro*ultimo";
+    primoLi.innerText = "mela*primo";
+    container.appendChild(ultimoLi);
+    container.insertBefore(primoLi, firstLi);
+});
